eae039ac70bb6745fbba1f66afad7ffcba3fd689
Caroko <92933685+Carokopamungkas@users.noreply.github.com>
2022-07-04 20:09:11 +0700

    uas pbo

---
+++ uas/src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+import java.awt.Dimension
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import java.awt.event.*;
+import javax.swing.*;
+
+
+fun main(args: Array<String>) {
+
+
+    val frame = JFrame("Hallo")
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.setSize(Dimension(600, 400))
+    frame.setLocationRelativeTo(null)
+    frame.setVisible(true)
+
+    val label = JLabel("caroko aji pamungkas")
+    label = 
+    label.setSize(Dimension(50, 100))
+    label.setVisible(true)
+
+
+
+
+
+
+
+}
